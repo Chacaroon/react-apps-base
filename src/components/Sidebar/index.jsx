336b5740af5942b98577ee1dc0d382efd337d221
Chacaroon <tribunskiy.chacaroon@gmail.com>
2017-09-24 14:26:20 +0300

    Structure main page

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import {Col} from 'react-bootstrap'
+
+class Sidebar extends Component {
+	render() {
+		return <Col lg={3} md={3} sm={3} xs={3}>
+			Sidebar
+		</Col>
+	}
+}
+
+export default Sidebar
